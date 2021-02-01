129cad59abe9b976f3fb0ff1618bf39ec71e0ecf
mohan8727 <45478292+mohan8727@users.noreply.github.com>
2021-02-01 18:14:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-	var x = document.getElementById('mybtn');
-			x.addEventListener('click', myfunc);
-			x.addEventListener('click', func1);
-			function myfunc() {
-				alert('this is for first click');
-			}
-			function func1() {
-				alert('this is  for second click');
-			}
+console.log("here is the hoised one");
